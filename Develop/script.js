12cf4136ddc9406709b1568e1f55bec001cffa6c
Huertz <104065687+Huertz@users.noreply.github.com>
2022-08-12 20:22:17 -0500

    Comment changed

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,34 @@
 // Assignment code here
+var special = [ 
+  ' ', ' ! ', ' " ', ' # ', ' $ ', ' % ',
+  ' & ', " ' ", ' ( ', ' ) ', ' * ', ' + ',
+  ' , ', ' - ', ' . ', ' / ', ' : ', ' ; ',
+  ' < ', ' = ', ' > ', ' ? ', ' @ ', ' [ ',
+  ' \ ', ' ] ', ' ^ ', ' _ ', ' ` ',' { ',
+  ' } ', ' ~ '] ;
 
+var num = [ 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+
+var upperCase = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+  'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+  'W', 'X', 'Y', 'Z'];
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+'v', 'w', 'x', 'y', 'z'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
+  window.prompt("How many characters would you like your password to contain");
+  
   passwordText.value = password;
 
 }

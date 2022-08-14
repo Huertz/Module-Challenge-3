eec8969dac9e54be313ad9269e503f5764bc66f2
Huertz <104065687+Huertz@users.noreply.github.com>
2022-08-14 15:44:29 -0500

    Moved html to the orignal spot

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,80 @@
-// Assignment code here
-var special = [ 
-  ' ', ' ! ', ' " ', ' # ', ' $ ', ' % ',
-  ' & ', " ' ", ' ( ', ' ) ', ' * ', ' + ',
-  ' , ', ' - ', ' . ', ' / ', ' : ', ' ; ',
-  ' < ', ' = ', ' > ', ' ? ', ' @ ', ' [ ',
-  ' \ ', ' ] ', ' ^ ', ' _ ', ' ` ',' { ',
-  ' } ', ' ~ '] ;
-
-var num = [ 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-
-var upperCase = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-  'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-  'W', 'X', 'Y', 'Z'];
-
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-'v', 'w', 'x', 'y', 'z'];
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-  window.prompt("How many characters would you like your password to contain");
-  
+
   passwordText.value = password;
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+  var lt = 0;
+  while (1) {
+    lt = prompt("How many characters would you like your password to contain?");
+    if (isNaN(lt) || lt < 8 || lt >= 128) {
+      alert("must be more than 8 characters and a number ");
+    }
+    else break;
+
+  }
+
+  var result = '';
+  var characters = '';
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const special = '~!@#$%^&*()_+}{:"<>?,./;';
+  const num = '0123456789';
+  const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+  if (confirm("Click Ok to confirm including special characters!")) {
+    for (let i = 0; i < special.length; i++) {
+      characters = characters.concat(special[i]);
+    }
+  }
+  if (confirm("Click Ok to confirm including numbers!")) {
+    for (let i = 0; i < num.length; i++) {
+      characters = characters.concat(num[i]);
+    }
+  }
+
+  if (confirm("Click Ok to confirm including lowercase characters!")) {
+    for (let i = 0; i < lowerCase.length; i++) {
+      characters = characters.concat(lowerCase[i]);
+    }
+  }
+
+  if (confirm("Click Ok to confirm including uppercase characters!")) {
+    for (let i = 0; i < upperCase.length
+
+
+      ; i++) {
+      characters = characters.concat(upperCase[i]);
+    }
+  }
+
+
+
+  var charactersLength = characters.length;
+  for (var i = 0; i < lt; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  /* Testing outputs
+  console.log(special);
+  console.log(characters);
+  console.log(result);
+  */
+  return result;
+
+
+}
+
